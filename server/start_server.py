7038c4b33f0ba54c010abd5d9b2ae9cf2a5ededc
Aditya Kasliwal <kasliwaladitya17@gmail.com>
2023-04-01 19:04:55 +0530

    Update start_server.py

--- server/start_server.py
+++ server/start_server.py
@@ -7,7 +7,7 @@ from server import server_start
 parser = argparse.ArgumentParser()
 parser.add_argument("--algorithm", type=str, default = "fedavg", help="Aggregation algorithm")
 parser.add_argument("--clients", type=int, default = 1, help="Number of clients to select")
-parser.add_argument("--fraction", type = float,default = 1, #Default value for fraction is 1, which means all the clients are selected help = "Fraction of clients to select out of the number provided or those available. Float between 0 to 1 inclusive")
+parser.add_argument("--fraction", type = float,default = 1,help = "Fraction of clients to select out of the number provided or those available. Float between 0 to 1 inclusive") #Default value for fraction is 1, which means all the clients are selected 
 parser.add_argument("--rounds", type=int, default = 1, help = "Total number of communication rounds to perform")
 parser.add_argument("--model_path", default = "test_model.pt",help = "The path of the initial server model's state dict")
 parser.add_argument("--epochs", type = int, default = 1, help="Number of epochs each client should perform in each round")
