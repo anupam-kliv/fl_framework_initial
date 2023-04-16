b94f3bca975ea5bc2b873dc40efe2fdf25cdb568
Aditya Kasliwal <kasliwaladitya17@gmail.com>
2023-04-16 21:25:47 +0530

    Update test_datasets.py

--- client/net.py
+++ client/net.py
@@ -4,34 +4,16 @@ import torch.nn.functional as F
 #from task_5.server.get_config import get_config
 import torchvision.models as models
 
-'''class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
-
-    def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
-        return x'''
-    
 class LeNet(nn.Module):
-    def __init__(self):
+    def __init__(self, in_channels=1, num_classes=10):
         super(LeNet, self).__init__()
-        self.conv1 = nn.Conv2d(1, 6, kernel_size=5)
+        self.conv1 = nn.Conv2d(in_channels, 6, kernel_size=5)
         self.pool1 = nn.MaxPool2d(kernel_size=2,stride=2)
         self.conv2 = nn.Conv2d(6, 16, kernel_size=5)
         self.pool2 = nn.MaxPool2d(kernel_size=2,stride=2)        
         self.fc1 = nn.Linear(400, 120)
         self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.fc3 = nn.Linear(84, num_classes)
         self.relu = nn.ReLU()
         self.logSoftmax = nn.LogSoftmax(dim=1)
 
@@ -51,19 +33,41 @@ class LeNet(nn.Module):
         x = self.logSoftmax(x)
         return x
 
-#config = get_config()
-
-
 def get_net(config):
-    if config['net'] == 'LeNet':
-        net = LeNet()
-    if config['net'] == 'resnet18':
-        net = models.resnet18()
-    if config['net'] == 'resnet50':
-        net = models.resnet18()
-    if config['net'] == 'vgg16':
-        net = models.vgg16()
+    if config["net"] == 'LeNet':
+        if config['dataset'] in ['MNIST', 'FashionMNIST']:
+            net = LeNet(in_channels=1, num_classes=10)
+        elif config['dataset'] == 'CIFAR10':
+            net = LeNet(in_channels=3, num_classes=10)
+        else:
+            net = LeNet(in_channels=3, num_classes=100)
+    if config["net"] == 'resnet18':
+        if config['dataset'] in ['MNIST', 'FashionMNIST']:
+            net = models.resnet18(num_classes=10)
+        elif config['dataset'] == 'CIFAR10':
+            net = models.resnet18(num_classes=10)
+        else:
+            net = models.resnet18(num_classes=100)
+    if config["net"] == 'resnet50':
+        if config['dataset'] in ['MNIST', 'FashionMNIST']:
+            net = models.resnet50(num_classes=10)
+        elif config['dataset'] == 'CIFAR10':
+            net = models.resnet50(num_classes=10)
+        else:
+            net = models.resnet50(num_classes=100)
+    if config["net"] == 'vgg16':
+        if config['dataset'] in ['MNIST', 'FashionMNIST']:
+            net = models.vgg16(num_classes=10)
+        elif config['dataset'] == 'CIFAR10':
+            net = models.vgg16(num_classes=10)
+        else:
+            net = models.vgg16(num_classes=100)
     if config['net'] == 'AlexNet':
-        net = models.AlexNet()
+        if config['dataset'] in ['MNIST', 'FashionMNIST']:
+            net = models.alexnet(num_classes=10)
+        elif config['dataset'] == 'CIFAR10':
+            net = models.alexnet(num_classes=10)
+        else:
+            net = models.alexnet(num_classes=100)
     print('model is loaded')
     return net
--- test/test_datasets.py
+++ test/test_datasets.py
@@ -7,6 +7,7 @@ import os
 # add main directory to path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))            
 from server.src.server import server_start  
+from server.src.server_lib import save_intial_model
 import time    
                                                              
 # This block of code enables us to call the script from command line.                                                                                
@@ -20,37 +21,74 @@ def execute(process):
         time.sleep(60)
         os.system(f'{y}')                                                                                                 
  
-def create_train_test_for_datasets():
+def create_train_test_for_MNIST():
     class TrainerTest(unittest.TestCase):
         @classmethod
         def setUpClass(cls):
-            config = get_config('test_algorithms', 'fedavg')
-            cls.config = config
+            config = get_config('test_datasets', 'MNIST')
+            save_intial_model(config)
 
         def test_MNIST(self):
             all_processes = ('server:MNIST', 'client:cd client ;python client.py')                                                                       
             process_pool = multiprocessing.Pool(processes = 2)                                                        
             process_pool.map(execute, all_processes)
 
-        def test_FashionMNIST(self):
+    return TrainerTest
+
+def create_train_test_for_FashionMnist():
+    class TrainerTest(unittest.TestCase):
+        @classmethod
+        def setUpClass(cls):
+            config = get_config('test_datasets', 'FashionMNIST')
+            save_intial_model(config)
+
+        def test_FashionMnist(self):
             all_processes = ('server:FashionMNIST', 'client:cd client ;python client.py')                                                                       
             process_pool = multiprocessing.Pool(processes = 2)                                                        
             process_pool.map(execute, all_processes)
 
-        def test_CIFAR10(self):
+    return TrainerTest
+
+def create_train_test_for_CIFAR10():
+    class TrainerTest(unittest.TestCase):
+        @classmethod
+        def setUpClass(cls):
+            config = get_config('test_datasets', 'CIFAR10')
+            save_intial_model(config)
+
+        def test_FashionMnist(self):
             all_processes = ('server:CIFAR10', 'client:cd client ;python client.py')                                                                       
             process_pool = multiprocessing.Pool(processes = 2)                                                        
             process_pool.map(execute, all_processes)
 
-        def test_CIFAR100(self):
+    return TrainerTest
+
+def create_train_test_for_CIFAR100():
+    class TrainerTest(unittest.TestCase):
+        @classmethod
+        def setUpClass(cls):
+            config = get_config('test_datasets', 'CIFAR100')
+            save_intial_model(config)
+
+        def test_FashionMnist(self):
             all_processes = ('server:CIFAR100', 'client:cd client ;python client.py')                                                                       
             process_pool = multiprocessing.Pool(processes = 2)                                                        
             process_pool.map(execute, all_processes)
 
     return TrainerTest
 
-class TestTrainer(create_train_test_for_datasets()):
-    'Test case for all datasets'
+
+class TestTrainer1(create_train_test_for_MNIST()):
+    'Test case for MNIST dataset'
+
+class TestTrainer2(create_train_test_for_FashionMnist()):
+    'Test case for FashionMNIST dataset'
+
+class TestTrainer3(create_train_test_for_CIFAR10()):
+    'Test case for CIFAR10 dataset'
+
+class TestTrainer4(create_train_test_for_CIFAR100()):
+    'Test case for CIFAR100 dataset'
 
 if __name__ == '__main__':
 
