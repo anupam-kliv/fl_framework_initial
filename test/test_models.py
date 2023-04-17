f7db8bbeea0e38538959b852b7386e0f9650b74b
Aditya Kasliwal <kasliwaladitya17@gmail.com>
2023-04-17 19:51:34 +0530

    Update test_models.py

--- test/test_models.py
+++ test/test_models.py
@@ -18,7 +18,7 @@ def execute(process):
         server_start(config)
     else:
         ##sleep 10 seconds to allow server to start
-        time.sleep(10)
+        time.sleep(5)
         os.system(f'{y}')                                                                                                 
  
 def create_train_test_for_LeNet():
