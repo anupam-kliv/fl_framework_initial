59ef91b8f9e7ca9bdeae2b30b3d9db39ec426cda
Aditya Kasliwal <kasliwaladitya17@gmail.com>
2023-04-17 20:21:53 +0530

    Update test_modules.py

--- test/test_modules.py
+++ test/test_modules.py
@@ -14,18 +14,22 @@ import time
 def execute(process):
     x,y = process.split(':')
     if x =='server':
-        config = get_config('test_models', y)
+        config = get_config('test_modules', y)
         server_start(config)
-    else:
+    elif x == 'client':
         ##sleep 10 seconds to allow server to start
         time.sleep(10)
-        os.system(f'{y}')                                                                                                 
+        os.system(f'{y}')   
+    else:
+        ##sleep 60 seconds to allow client to join after server has started training
+        time.sleep(60)
+        os.system(f'{y}')                                                                                              
  
 def create_train_test_for_verification_module():
     class TrainerTest(unittest.TestCase):
         @classmethod
         def setUpClass(cls):
-            config = get_config('test_models', 'LeNet')
+            config = get_config('test_modules', 'verification')
             save_intial_model(config)
 
         def test_verification_module(self):
@@ -35,8 +39,42 @@ def create_train_test_for_verification_module():
 
     return TrainerTest
 
+def create_train_test_for_timeout_module():
+    class TrainerTest(unittest.TestCase):
+        @classmethod
+        def setUpClass(cls):
+            config = get_config('test_modules', 'timeout')
+            save_intial_model(config)
+
+        def test_verification_module(self):
+            all_processes = ('server:timeout', 'client:cd client ;python client.py', 'client:cd client ;python client.py')                                                                       
+            process_pool = multiprocessing.Pool(processes = 3)                                                        
+            process_pool.map(execute, all_processes)
+
+    return TrainerTest
+
+def create_train_test_for_intermediate_connection_module():
+    class TrainerTest(unittest.TestCase):
+        @classmethod
+        def setUpClass(cls):
+            config = get_config('test_modules', 'intermediate')
+            save_intial_model(config)
+
+        def test_verification_module(self):
+            all_processes = ('server:intermediate', 'client:cd client ;python client.py', 'late_client:cd client ;python client.py')                                                                       
+            process_pool = multiprocessing.Pool(processes = 3)                                                        
+            process_pool.map(execute, all_processes)
+
+    return TrainerTest
+
 class TestTrainer_verification(create_train_test_for_verification_module()):
     'Test case for verification module'
+
+class TestTrainer_timeout(create_train_test_for_timeout_module()):
+    'Test case for timeout module'
+
+class TestTrainer_intermediate(create_train_test_for_intermediate_connection_module()):
+    'Test case for intermediate client connections module'
 
 if __name__ == '__main__':
 
