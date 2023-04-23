d2b45ca1288c1861c8b64b346912a321b24cc7f1
Aditya Kasliwal <kasliwaladitya17@gmail.com>
2023-04-23 22:00:49 +0530

    Update distribution.py

--- client/distribution.py
+++ client/distribution.py
@@ -1,42 +1,54 @@
 import numpy as np
 import torch, random, os
-import matplotlib.pyplot as plt
 
 def data_distribution(config, trainset):
     labels = []
     base_dir = os.getcwd()
     storepath = os.path.join(base_dir, 'Distribution/', config['dataset']+'/')
+    seed = 10
+    random.seed(seed)
+    num_users = 2
+    
+    #Calculate the number of samples present per class
     for i in range(len(trainset)):
         labels.append(trainset[i][1])
-    #print(trainset)
     unique_labels = np.unique(np.array(labels))
     label_index_list = {}
     for key in unique_labels:
         label_index_list[key] = []
     for index, label in enumerate(labels):
         label_index_list[label].append(index)
-    
-    seed = 10
-    random.seed(seed)
-    num_users = 150
     num_classes = len(unique_labels)
+    
+    #Calculate the value of the probability distribution. For K=1, it will be iid distribution
     K = config['niid']
-    #print(K)
-    q_step = (1 - (1/num_classes))/(K-1)
-    print(q_step)
+    if (K==1):
+    	q_step = (1 - (1/num_classes))
+    else:
+    	q_step = (1 - (1/num_classes))/(K-1)
+    
+    #Shuffle the index position for all classes
     for i in range(len(label_index_list)):
         random.shuffle(label_index_list[i])
+        
+    #Generate the different non-iid distribution. Data_presence_indicator will help to reduce the number of classes among the clients as the non-iid increases   
     for j in range(K):
         dist = np.random.uniform(q_step, (1+j)*q_step, (num_classes, num_users))
         if j != 0:
             data_presence_indicator = np.random.choice([0, 1], (num_classes, num_users), p=[j*q_step, 1-(j*q_step)])
+            if len(np.where(np.sum(data_presence_indicator, axis=0) == 0)[0])>0:
+                for i in np.where(np.sum(data_presence_indicator, axis=0) == 0)[0]:
+                    print("Zero client number: ", i)
+                    zero_array = data_presence_indicator[:,i]
+                    zero_array[np.random.choice(len(zero_array),1)] =1
+                    data_presence_indicator[:,i] = zero_array
             dist = np.multiply(dist,data_presence_indicator)
-        psum = np.sum(dist, axis=1)
+        psum = np.sum(dist, axis=1) 
         for i in range(dist.shape[0]):
-            dist[i] = dist[i]*len(label_index_list[i])/psum[i]
+            dist[i] = dist[i]*len(label_index_list[i])/(psum[i]+0.00001)
         dist = np.floor(dist).astype(int)
         
-        ## If any client does not get any data
+        # If any client does not get any data then this logic helps to allocate the required samples among the clients
         gainers = list(np.where(np.sum(dist, axis=0) != 0))[0]
         if len(gainers) < num_users:
             losers = list(np.where(np.sum(dist, axis=0) == 0))[0]
@@ -49,6 +61,7 @@ def data_distribution(config, trainset):
                     dist[digit, donor] = dist[digit, donor] - num_transfer
                     dist[digit, losers[index]] = num_transfer
         
+        #Logic to check if the summation of all the samples among the clients is equal to the total number of samples present for that class. If not it will adjust. 
         for num in range(num_classes):
             while dist[num].sum() != len(label_index_list[num]):
                 index = random.randint(0,num_users-1)
@@ -57,6 +70,7 @@ def data_distribution(config, trainset):
                 else:
                     dist[num][index]-=1
         
+        #Division of samples number among the clients
         split = [[] for i in range(num_classes)]
         for num in range(num_classes):
             start = 0
@@ -64,6 +78,7 @@ def data_distribution(config, trainset):
                 split[num].append(label_index_list[num][start:start+dist[num][i]])
                 start = start+dist[num][i]
         
+        #Division of actual data points among the clients.
         datapoints = [[] for i in range(num_users)]
         class_histogram = [[] for i in range(num_users)]
         class_stats= [[] for i in range(num_users)]
@@ -75,13 +90,12 @@ def data_distribution(config, trainset):
                     class_stats[i].append(0)
                 else:
                     class_stats[i].append(1)
-                
-        #file_name = storepath 
+        
+        #Store the dataset division in the folder        
         if not os.path.exists(storepath):
             os.makedirs(storepath)
         file_name = 'data_split_niid_'+ str(K)+'.pt'
 
-        #print(storepath+file_name)
         torch.save({'datapoints': datapoints, 'histograms': class_histogram, 'class_statitics': class_stats}, storepath + file_name)
         
     
