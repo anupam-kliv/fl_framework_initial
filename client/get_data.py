0f1211e19c97e8203f21ffd694eb99933d687e36
Aditya Kasliwal <kasliwaladitya17@gmail.com>
2023-04-23 22:49:06 +0530

    Update get_data.py

--- client/get_data.py
+++ client/get_data.py
@@ -14,24 +14,26 @@ import torchvision
 
 
 def get_data(config):
-    #print(config)
+    dataset_path="./dataset"
+    if not os.path.exists(dataset_path):
+        os.makedirs(dataset_path)  
     if config['dataset'] == 'MNIST':
         apply_transform = transforms.Compose([transforms.Resize(config["resize_size"]), transforms.ToTensor()])
-        trainset = datasets.MNIST(root='./MNIST', train=True, download=True, transform=apply_transform)
-        testset = datasets.MNIST(root='./MNIST', train=False, download=True, transform=apply_transform)
+        trainset = datasets.MNIST(root='./dataset/MNIST', train=True, download=True, transform=apply_transform)
+        testset = datasets.MNIST(root='./dataset/MNIST', train=False, download=True, transform=apply_transform)
     if config['dataset'] == 'FashionMNIST':
         apply_transform = transforms.Compose([transforms.Resize(config['resize_size']), transforms.ToTensor()])
-        trainset = datasets.FashionMNIST(root='./FashionMNIST', train=True, download=True, transform=apply_transform)
-        testset = datasets.FashionMNIST(root='./FashionMNIST', train=False, download=True, transform=apply_transform)
+        trainset = datasets.FashionMNIST(root='./dataset/FashionMNIST', train=True, download=True, transform=apply_transform)
+        testset = datasets.FashionMNIST(root='./dataset/FashionMNIST', train=False, download=True, transform=apply_transform)
 
     if config['dataset'] == 'CIFAR10':
         apply_transform = transforms.Compose([transforms.Resize(config['resize_size']), transforms.ToTensor()])
-        trainset = datasets.CIFAR10(root='./CIFAR10', train=True, download=True, transform=apply_transform)
-        testset = datasets.CIFAR10(root='./CIFAR10', train=False, download=True, transform=apply_transform)
+        trainset = datasets.CIFAR10(root='./dataset/CIFAR10', train=True, download=True, transform=apply_transform)
+        testset = datasets.CIFAR10(root='./dataset/CIFAR10', train=False, download=True, transform=apply_transform)
 
     if config['dataset'] == 'CIFAR100':
         apply_transform = transforms.Compose([transforms.Resize(config['resize_size']), transforms.ToTensor()])
-        trainset = datasets.CIFAR100(root='./CIFAR100', train=True, download=True, transform=apply_transform)
-        testset = datasets.CIFAR100(root='./CIFAR100', train=False, download=True, transform=apply_transform)
+        trainset = datasets.CIFAR100(root='./dataset/CIFAR100', train=True, download=True, transform=apply_transform)
+        testset = datasets.CIFAR100(root='./dataset/CIFAR100', train=False, download=True, transform=apply_transform)
 
     return trainset, testset
