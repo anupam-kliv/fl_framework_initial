c25bc3e3e6ac53212dedfe39daa0917cd827b71b
Aditya Kasliwal <kasliwaladitya17@gmail.com>
2023-04-25 17:56:15 +0530

    Update test_scalability.py

--- test/test_scalability.py
+++ test/test_scalability.py
@@ -13,7 +13,7 @@ def create_train_test_for_two_clients():
             config = get_config('test_scalability', '2')
             save_intial_model(config)
 
-        def test_timeout_module(self):
+        def test_two_clients(self):
             config = get_config('test_scalability', '2')
             tester(config,2)
 
@@ -26,7 +26,7 @@ def create_train_test_for_four_clients():
             config = get_config('test_scalability', '4')
             save_intial_model(config)
 
-        def test_intermediate_client_connections_module(self):
+        def test_four_clients(self):
             config = get_config('test_scalability', '4')
             tester(config,4)
 
@@ -39,7 +39,7 @@ def create_train_test_for_six_clients():
             config = get_config('test_scalability', '6')
             save_intial_model(config)
 
-        def test_intermediate_client_connections_module(self):
+        def test_six_clients(self):
             config = get_config('test_scalability', '6')
             tester(config,6)
 
@@ -52,7 +52,7 @@ def create_train_test_for_eight_clients():
             config = get_config('test_scalability', '8')
             save_intial_model(config)
 
-        def test_intermediate_client_connections_module(self):
+        def test_eight_clients(self):
             config = get_config('test_scalability', '8')
             tester(config,8)
 
@@ -65,9 +65,48 @@ def create_train_test_for_ten_clients():
             config = get_config('test_scalability', '10')
             save_intial_model(config)
 
-        def test_intermediate_client_connections_module(self):
+        def test_ten_clients(self):
             config = get_config('test_scalability', '10')
             tester(config,10)
+
+    return TrainerTest
+
+def create_train_test_for_five_rounds():
+    class TrainerTest(unittest.TestCase):
+        @classmethod
+        def setUpClass(cls):
+            config = get_config('test_scalability', '5_rounds')
+            save_intial_model(config)
+
+        def test_five_communication_rounds(self):
+            config = get_config('test_scalability', '5_rounds')
+            tester(config,2)
+
+    return TrainerTest
+
+def create_train_test_for_ten_rounds():
+    class TrainerTest(unittest.TestCase):
+        @classmethod
+        def setUpClass(cls):
+            config = get_config('test_scalability', '10_rounds')
+            save_intial_model(config)
+
+        def test_ten_communication_rounds(self):
+            config = get_config('test_scalability', '10_rounds')
+            tester(config,2)
+
+    return TrainerTest
+
+def create_train_test_for_twenty_rounds():
+    class TrainerTest(unittest.TestCase):
+        @classmethod
+        def setUpClass(cls):
+            config = get_config('test_scalability', '20_rounds')
+            save_intial_model(config)
+
+        def test_twenty_communication_rounds(self):
+            config = get_config('test_scalability', '20_rounds')
+            tester(config,2)
 
     return TrainerTest
 
@@ -85,6 +124,15 @@ class TestTrainer_8(create_train_test_for_eight_clients()):
 
 class TestTrainer_10(create_train_test_for_ten_clients()):
     'Test case for ten clients'
+
+class TestTrainer_5_rounds(create_train_test_for_five_rounds()):
+    'Test case for five communication rounds'
+
+class TestTrainer_10_rounds(create_train_test_for_ten_rounds()):
+    'Test case for ten communication rounds'
+
+class TestTrainer_20_rounds(create_train_test_for_twenty_rounds()):
+    'Test case for twenty communication rounds'
 
 if __name__ == '__main__':
 
