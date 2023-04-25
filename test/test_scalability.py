bc7e5f92c86e96013b8718fe077ce74a7c02a7e5
Aditya Kasliwal <kasliwaladitya17@gmail.com>
2023-04-25 17:47:43 +0530

    Update test_scalability.py

--- test/test_scalability.py
+++ test/test_scalability.py
@@ -2,24 +2,9 @@ import unittest
 import os
 import sys
 from misc import get_config, tester
-import multiprocessing
 import os    
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))            
 from server.src.server_lib import save_intial_model
-import time                                                                                             
- 
-def create_train_test_for_one_client():
-    class TrainerTest(unittest.TestCase):
-        @classmethod
-        def setUpClass(cls):
-            config = get_config('test_scalability', '1')
-            save_intial_model(config)
-
-        def test_verification_module(self):
-            config = get_config('test_scalability', '1')
-            tester(config,1)
-
-    return TrainerTest
 
 def create_train_test_for_two_clients():
     class TrainerTest(unittest.TestCase):
@@ -31,19 +16,6 @@ def create_train_test_for_two_clients():
         def test_timeout_module(self):
             config = get_config('test_scalability', '2')
             tester(config,2)
-
-    return TrainerTest
-
-def create_train_test_for_three_clients():
-    class TrainerTest(unittest.TestCase):
-        @classmethod
-        def setUpClass(cls):
-            config = get_config('test_scalability', '3')
-            save_intial_model(config)
-
-        def test_intermediate_client_connections_module(self):
-            config = get_config('test_scalability', '3')
-            tester(config,3)
 
     return TrainerTest
 
@@ -60,17 +32,59 @@ def create_train_test_for_four_clients():
 
     return TrainerTest
 
-class TestTrainer_1(create_train_test_for_one_client()):
-    'Test case for one client'
+def create_train_test_for_six_clients():
+    class TrainerTest(unittest.TestCase):
+        @classmethod
+        def setUpClass(cls):
+            config = get_config('test_scalability', '6')
+            save_intial_model(config)
+
+        def test_intermediate_client_connections_module(self):
+            config = get_config('test_scalability', '6')
+            tester(config,6)
+
+    return TrainerTest
+
+def create_train_test_for_eight_clients():
+    class TrainerTest(unittest.TestCase):
+        @classmethod
+        def setUpClass(cls):
+            config = get_config('test_scalability', '8')
+            save_intial_model(config)
+
+        def test_intermediate_client_connections_module(self):
+            config = get_config('test_scalability', '8')
+            tester(config,8)
+
+    return TrainerTest
+
+def create_train_test_for_ten_clients():
+    class TrainerTest(unittest.TestCase):
+        @classmethod
+        def setUpClass(cls):
+            config = get_config('test_scalability', '10')
+            save_intial_model(config)
+
+        def test_intermediate_client_connections_module(self):
+            config = get_config('test_scalability', '10')
+            tester(config,10)
+
+    return TrainerTest
 
 class TestTrainer_2(create_train_test_for_two_clients()):
     'Test case for two clients'
 
-class TestTrainer_3(create_train_test_for_three_clients()):
-    'Test case for three clients'
-
 class TestTrainer_4(create_train_test_for_four_clients()):
     'Test case for four clients'
+
+class TestTrainer_6(create_train_test_for_six_clients()):
+    'Test case for six clients'
+
+class TestTrainer_8(create_train_test_for_eight_clients()):
+    'Test case for eight clients'
+
+class TestTrainer_10(create_train_test_for_ten_clients()):
+    'Test case for ten clients'
 
 if __name__ == '__main__':
 
