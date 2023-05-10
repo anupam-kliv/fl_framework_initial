0c91ec32fe5acb26288f04415f13c6812894f78a
anupam-kliv <anupam.borthakur12@gmail.com>
2023-05-10 18:15:57 +0530

    Update test_models.py

--- test/test_models.py
+++ test/test_models.py
@@ -1,11 +1,17 @@
-from server.src.server_lib import save_intial_model
-import unittest
 import os
 import sys
+import unittest
+from server.src.server_lib import save_intial_model
 from misc import get_config, tester
 # add main directory to path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
 def create_train_test_for_LeNet():
+    '''
+    This function us used to verify the LeNet-5 CNN model 
+    using one client
+    '''
     class TrainerTest(unittest.TestCase):
         @classmethod
         def setUpClass(cls):
@@ -13,12 +19,17 @@ def create_train_test_for_LeNet():
             save_intial_model(config['server'])
 
         def test_LeNet(self):
-            print("\n==========================LeNet Testing==========================")
+            print("\n======================LeNet Testing======================")
             config = get_config('test_models', 'LeNet')
-            tester(config,1)
+            tester(config, 1)
     return TrainerTest
+
 
 def create_train_test_for_resnet18():
+    '''
+    This function us used to verify the ResNet-18 CNN model 
+    using one client
+    '''
     class TrainerTest(unittest.TestCase):
         @classmethod
         def setUpClass(cls):
@@ -26,12 +37,17 @@ def create_train_test_for_resnet18():
             save_intial_model(config['server'])
 
         def test_resnet18(self):
-            print("\n==========================Resnet18 Testing==========================")
+            print("\n======================Resnet18 Testing======================")
             config = get_config('test_models', 'resnet18')
-            tester(config,1)
+            tester(config, 1)
     return TrainerTest
+
 
 def create_train_test_for_resnet50():
+    '''
+    This function us used to verify the ResNet-50 CNN model 
+    using one client
+    '''    
     class TrainerTest(unittest.TestCase):
         @classmethod
         def setUpClass(cls):
@@ -39,12 +55,17 @@ def create_train_test_for_resnet50():
             save_intial_model(config['server'])
 
         def test_resnet18(self):
-            print("\n==========================Resnet50 Testing==========================")
+            print("\n======================Resnet50 Testing======================")
             config = get_config('test_models', 'resnet50')
             tester(config,1)
     return TrainerTest
 
+
 def create_train_test_for_vgg16():
+    '''
+    This function us used to verify the VGG-16 CNN model 
+    using one client
+    '''
     class TrainerTest(unittest.TestCase):
         @classmethod
         def setUpClass(cls):
@@ -52,12 +73,17 @@ def create_train_test_for_vgg16():
             save_intial_model(config['server'])
 
         def test_vgg16(self):
-            print("\n==========================VGG 16 Testing==========================")
+            print("\n======================VGG 16 Testing======================")
             config = get_config('test_models', 'vgg16')
-            tester(config,1)
+            tester(config, 1)
     return TrainerTest
 
+
 def create_train_test_for_AlexNet():
+    '''
+    This function us used to verify the AlexNet CNN model 
+    using one client
+    '''
     class TrainerTest(unittest.TestCase):
         @classmethod
         def setUpClass(cls):
@@ -65,9 +91,9 @@ def create_train_test_for_AlexNet():
             save_intial_model(config['server'])
 
         def test_vgg16(self):
-            print("\n==========================AlexNet Testing==========================")
+            print("\n======================AlexNet Testing======================")
             config = get_config('test_models', 'AlexNet')
-            tester(config,1)
+            tester(config, 1)
     return TrainerTest
 
 class TestTrainer_LeNet(create_train_test_for_LeNet()):
