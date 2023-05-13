852baf360090bb0d94d5020fce3d6dfcced6fa70
Aditya Kasliwal <kasliwaladitya17@gmail.com>
2023-05-13 12:08:19 +0530

    Update test_modules.py

--- test/test_modules.py
+++ test/test_modules.py
@@ -2,10 +2,11 @@ import os
 import unittest
 import sys
 
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from server.src.server_lib import save_intial_model
 from misc import get_config, tester
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 
 
 def create_train_test_for_verification_module():
