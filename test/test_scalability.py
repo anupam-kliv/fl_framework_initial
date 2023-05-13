6deacda7c3f270e8822417de01711c841ff174fb
anupam-kliv <anupam.borthakur12@gmail.com>
2023-05-13 10:46:50 +0530

    Update test_scalability.py

--- test/test_scalability.py
+++ test/test_scalability.py
@@ -45,43 +45,6 @@ def create_train_test_for_six_clients():
     return TrainerTest
 
 
-def create_train_test_for_eight_clients():
-    """
-    Verify the scalability of clients using eight clients by implementing the following function
-    """
-    
-    class TrainerTest(unittest.TestCase):
-        @classmethod
-        def setUpClass(cls):
-            config = get_config('test_scalability', '8')
-            save_intial_model(config['server'])
-
-        def test_eight_clients(self):
-            print("\n=== Testing  for #client=8 ==")
-            config = get_config('test_scalability', '8')
-            tester(config, 8)
-
-    return TrainerTest
-
-
-def create_train_test_for_ten_clients():
-    """
-    Verify the scalability of clients using ten clients by implementing the following function
-    """
-    
-    class TrainerTest(unittest.TestCase):
-        @classmethod
-        def setUpClass(cls):
-            config = get_config('test_scalability', '10')
-            save_intial_model(config['server'])
-
-        def test_ten_clients(self):
-            print("\n== Testing  for #client=10 ==")
-            config = get_config('test_scalability', '10')
-            tester(config, 10)
-
-    return TrainerTest
-
 
 def create_train_test_for_five_rounds():
     """
@@ -121,23 +84,6 @@ def create_train_test_for_ten_rounds():
     return TrainerTest
 
 
-def create_train_test_for_twenty_rounds():
-    """
-    Verify the scalability of CR rounds using twenty round by implementing the following function
-    """
-    
-    class TrainerTest(unittest.TestCase):
-        @classmethod
-        def setUpClass(cls):
-            config = get_config('test_scalability', '20_rounds')
-            save_intial_model(config['server'])
-
-        def test_twenty_communication_rounds(self):
-            print("\n== Testing  for Communication Rounds=20 ==")
-            config = get_config('test_scalability', '20_rounds')
-            tester(config, 2)
-
-    return TrainerTest
 
 
 class TestTrainer_4(create_train_test_for_four_clients()):
@@ -148,14 +94,6 @@ class TestTrainer_6(create_train_test_for_six_clients()):
     'Test case for six clients'
 
     
-class TestTrainer_8(create_train_test_for_eight_clients()):
-    'Test case for eight clients'
-
-    
-class TestTrainer_10(create_train_test_for_ten_clients()):
-    'Test case for ten clients'
-
-    
 class TestTrainer_5_rounds(create_train_test_for_five_rounds()):
     'Test case for five communication rounds'
 
@@ -164,8 +102,6 @@ class TestTrainer_10_rounds(create_train_test_for_ten_rounds()):
     'Test case for ten communication rounds'
 
     
-class TestTrainer_20_rounds(create_train_test_for_twenty_rounds()):
-    'Test case for twenty communication rounds'
 
     
 if __name__ == '__main__':
